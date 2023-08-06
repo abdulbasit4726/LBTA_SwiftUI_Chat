c581b265e319c1aa36c12722fc0dc03a3cd11499
Abdul Basit <abdul.basit@frizhub.com>
2023-08-06 14:49:03 +0500

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by frizhub on 06/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
